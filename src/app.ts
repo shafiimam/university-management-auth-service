a8f097114e15163aa93c9e1fb0b2d86dd7b3a8eb
Shah Md. Shafi Imam <71667417+shafiimam@users.noreply.github.com>
2023-06-10 02:52:50 +0600

    Setup eslint prettier husky and lint-staged

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
